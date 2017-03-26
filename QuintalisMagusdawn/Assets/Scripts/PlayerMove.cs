3532183a9a4044da4f5f7577faf859c1f6cce7ea
Quintavius <loths.marc@gmail.com>
2017-03-27 12:26:45 +1300

    gitkraken you fucking piece of shit

---
+++ QuintalisMagusdawn/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    public float moveSpeed = 1;
+
+	// Use this for initialization
+	void Start () {
+        CharacterController controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis("Horizontal") != 0)
+        {
+
+        }
+        if (Input.GetAxis("Vertical") != 0)
+        {
+
+        }
+	}
+}
