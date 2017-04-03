f8855207dad3c2981064882016f771c66079c3d9
Quintavius <loths.marc@gmail.com>
2017-04-04 08:55:31 +1200

    Keyboard controls

--- QuintalisMagusdawn/Assets/Scripts/PlayerMove.cs
+++ QuintalisMagusdawn/Assets/Scripts/PlayerMove.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
+    [Header("Character Values")]
     public float moveSpeed = 1;
-    public CharacterController controller;
-    public GameObject boom;
     public float lookSpeed = 1;
 
-	// Use this for initialization
-	void Start () {
+    [Header("Debug Values")]
+    public float hSpeed;
+    public float vSpeed;
+    public float hLook;
+    public CharacterController controller;
+    public GameObject boom;
+
+    // Use this for initialization
+    void Start () {
         //Grab controller
         controller = GetComponent<CharacterController>();
         boom = GameObject.FindWithTag("CameraBoom");
@@ -17,18 +23,40 @@ public class PlayerMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //Basic Movement
-        var hSpeed = Input.GetAxis("Horizontal");
-        var vSpeed = Input.GetAxis("Vertical");
- 
-        var moveDir = new Vector3(hSpeed, 0, vSpeed);                    //Give me a world based movement vector
-        moveDir = Camera.main.transform.TransformDirection(moveDir);     //Slap that shit on the camera
+        hSpeed = Input.GetAxis("Horizontal");
+        vSpeed = Input.GetAxis("Vertical");
+        if (hSpeed != 0 || vSpeed != 0)
+        {
+            MovePlayer();
+        }
+
+        hLook = Input.GetAxis("LookHorizontal");
+        if (hLook != 0)
+        {
+            MoveCamera();
+        }
+    }
+
+    void MovePlayer()
+    {
+        var moveDir = new Vector3(0, 0, 0);
+        if (Mathf.Abs(hSpeed) == 1 && Mathf.Abs(vSpeed) == 1)
+        {
+            //Keyboard Hack
+            moveDir = new Vector3(hSpeed * 0.75f, 0, vSpeed * 0.75f);
+        }
+        else
+        {
+            moveDir = new Vector3(hSpeed, 0, vSpeed);
+        }
+        moveDir = Camera.main.transform.TransformDirection(moveDir);
         controller.SimpleMove(moveDir * moveSpeed);                      //holy shit we movin
+    }
 
-        //Camera Control
-        var hLook = Input.GetAxis("LookHorizontal");
+    void MoveCamera()
+    {
         //var vLook = Input.GetAxis("LookVertical");                    //Let's not for now
-
+        //Move Boom, not Camera
         boom.transform.Rotate(0, hLook * lookSpeed, 0);
     }
 }
