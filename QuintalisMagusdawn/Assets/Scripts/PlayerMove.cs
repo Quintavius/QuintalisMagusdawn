dbb831966e2f10a4ed3db016a661f8fb55c03a9f
Quintavius <loths.marc@gmail.com>
2017-04-04 12:38:02 +1200

    Camera panning

--- QuintalisMagusdawn/Assets/Scripts/PlayerMove.cs
+++ QuintalisMagusdawn/Assets/Scripts/PlayerMove.cs
@@ -6,6 +6,15 @@ public class PlayerMove : MonoBehaviour {
     [Header("Character Values")]
     public float moveSpeed = 1;
     public float lookSpeed = 1;
+    public float hVelocity = 0f;
+    public float vVelocity = 0f;
+    public float maximumOffset = 4;
+    public float cameraDistance = -7.5f;
+    public float cameraHeight = 0.8f;
+    public AnimationCurve panCurve;
+    [SerializeField]
+    [Range(0f, 1f)]
+    public float lerpSpeed = 0.02f;
 
     [Header("Debug Values")]
     public float hSpeed;
@@ -13,12 +22,19 @@ public class PlayerMove : MonoBehaviour {
     public float hLook;
     public CharacterController controller;
     public GameObject boom;
+    public Vector3 cameraStart;
+    public Vector3 startOffset;
+    public Vector3 moveDir;
+    public Vector3 camPos;
+    public float hMod = 0f;
+    public float vMod = 0f;
 
     // Use this for initialization
     void Start () {
         //Grab controller
         controller = GetComponent<CharacterController>();
         boom = GameObject.FindWithTag("CameraBoom");
+        startOffset = Camera.main.transform.localPosition;
 	}
 	
 	// Update is called once per frame
@@ -33,13 +49,24 @@ public class PlayerMove : MonoBehaviour {
         hLook = Input.GetAxis("LookHorizontal");
         if (hLook != 0)
         {
-            MoveCamera();
+            RotateCamera();
         }
-    }
+
+        //Move the BOOM towards player movement
+        var camTarget = Vector3.zero;
+
+        hMod = Mathf.SmoothDamp(hMod,hSpeed, ref hVelocity,lerpSpeed);
+        vMod = Mathf.SmoothDamp(vMod, vSpeed, ref vVelocity, lerpSpeed);
+        camTarget = new Vector3(hMod * maximumOffset, 0,vMod * maximumOffset);
+        camTarget = boom.transform.TransformDirection(camTarget);
+
+        boom.transform.localPosition = camTarget;
+        boom.transform.localPosition += new Vector3(0, cameraHeight,0);
+    } 
 
     void MovePlayer()
     {
-        var moveDir = new Vector3(0, 0, 0);
+        moveDir = new Vector3(0, 0, 0);
         if (Mathf.Abs(hSpeed) == 1 && Mathf.Abs(vSpeed) == 1)
         {
             //Keyboard Hack
@@ -53,7 +80,7 @@ public class PlayerMove : MonoBehaviour {
         controller.SimpleMove(moveDir * moveSpeed);                      //holy shit we movin
     }
 
-    void MoveCamera()
+    void RotateCamera()
     {
         //var vLook = Input.GetAxis("LookVertical");                    //Let's not for now
         //Move Boom, not Camera
